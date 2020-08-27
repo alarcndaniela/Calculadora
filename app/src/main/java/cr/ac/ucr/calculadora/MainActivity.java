b2a66981337a6e22d99ef92fc7c3ad7455df1d84
Daniela Alarcón <daniela.alarcongamboa27@gmail.com>
2020-08-27 16:47:01 -0600

    First commit

--- app/src/main/java/cr/ac/ucr/calculadora/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/calculadora/MainActivity.java
@@ -6,21 +6,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import static java.lang.Math.floor;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button btn_uno, btn_dos, btn_tres, btn_cuatro, btn_cinco, btn_seis, btn_siete, btn_ocho, btn_nueve, btn_cero,
-            btn_sumar, btn_restar, btn_multiplicar, btn_dividir, btn_borrar, btn_punto, btn_igual;
+    //declaración
+    Button btn_uno, btn_dos,
+            btn_tres, btn_cuatro,
+            btn_cinco, btn_seis,
+            btn_siete, btn_ocho,
+            btn_nueve, btn_cero,
+            btn_sumar, btn_restar,
+            btn_multiplicar, btn_dividir,
+            btn_borrar, btn_punto,
+            btn_igual;
     TextView Resultado;
     double resultado;
-    String operador, mostrar, reserva;
+    String mostrar, memoria, operacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Botones
         btn_cero = (Button)findViewById(R.id.btn_cero);
         btn_uno = (Button)findViewById(R.id.btn_uno);
         btn_dos = (Button)findViewById(R.id.btn_dos);
@@ -31,17 +39,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_siete = (Button)findViewById(R.id.btn_siete);
         btn_ocho = (Button)findViewById(R.id.btn_ocho);
         btn_nueve = (Button)findViewById(R.id.btn_nueve);
+        btn_dividir = (Button)findViewById(R.id.btn_dividir);
+        btn_multiplicar = (Button)findViewById(R.id.btn_multiplicar);
         btn_sumar = (Button)findViewById(R.id.btn_sumar);
         btn_restar = (Button)findViewById(R.id.btn_restar);
-        btn_multiplicar = (Button)findViewById(R.id.btn_multiplicar);
-        btn_dividir = (Button)findViewById(R.id.btn_dividir);
         btn_borrar = (Button)findViewById(R.id.btn_borrar);
-        Resultado = (TextView)findViewById(R.id.area_resultado);
         btn_punto = (Button)findViewById(R.id.btn_punto);
         btn_igual = (Button)findViewById(R.id.btn_igual);
 
+        //TextView
+        Resultado = (TextView)findViewById(R.id.area_resultado);
     }
 
+    //método OnClick para los botones
     @Override
     public void onClick(View view){
         switch (view.getId()){
@@ -50,123 +60,149 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mostrar = mostrar + "0";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_uno:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "1";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_dos:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "2";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_tres:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "3";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_cuatro:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "4";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_cinco:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "5";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_seis:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "6";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_siete:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "7";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_ocho:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "8";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_nueve:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + "9";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_punto:
                 mostrar = Resultado.getText().toString();
                 mostrar = mostrar + ".";
                 Resultado.setText(mostrar);
                 break;
+
             case R.id.btn_borrar:
                 mostrar = "";
                 Resultado.setText(mostrar);
-                reserva = "";
-                operador = "";
+                memoria = "";
+                operacion = "";
                 break;
+
             case R.id.btn_sumar:
-                reserva = Resultado.getText().toString();
-                operador = "+";
+                memoria = Resultado.getText().toString();
+                operacion = "+";
                 Resultado.setText("");
                 break;
+
             case R.id.btn_restar:
-                reserva = Resultado.getText().toString();
-                operador = "-";
+                memoria = Resultado.getText().toString();
+                operacion = "-";
                 Resultado.setText("");
                 break;
+
             case R.id.btn_dividir:
-                reserva = Resultado.getText().toString();
-                operador = "/";
+                memoria = Resultado.getText().toString();
+                operacion = "/";
                 Resultado.setText("");
                 break;
+
             case R.id.btn_multiplicar:
-                reserva = Resultado.getText().toString();
-                operador = "*";
+                memoria = Resultado.getText().toString();
+                operacion = "x";
                 Resultado.setText("");
                 break;
+
             case R.id.btn_igual:
                 showResult();
                 break;
+
             default:
                 break;
         }
     }
 
     public void showResult(){
-            mostrar = Resultado.getText().toString();
-            if(operador.equals("-")){
-                resultado = Double.parseDouble(reserva) - Double.parseDouble(Resultado.getText().toString());
-                if(floor(resultado) == resultado){
-                    Resultado.setText(String.valueOf((int)Math.round(resultado)));
-                }else{
-                    Resultado.setText(String.valueOf(resultado));
-                }
+        mostrar = Resultado.getText().toString();
+        //EXPLICACIÓN DEL METODO SHOW RESULT
+
+        // se le da de valor a "resultado" el resultado de la "operación" entre el número que acaba de digitar el usuario
+        // y el número que digitó anteriormente (memoria), posteriormente mediante el método floor de la librería Math
+        // para ver si es necesario o no convertir el resultado a un entero, por ejemplo 2+2 no sería 4.0 (double) sino 4 (int)
+
+        if(operacion.equals("-")){
+            resultado = Double.parseDouble(memoria) - Double.parseDouble(Resultado.getText().toString());
+            if(floor(resultado) == resultado){
+                Resultado.setText(String.valueOf((int)Math.round(resultado)));
+            }else{
+                Resultado.setText(String.valueOf(resultado));
             }
-            if(operador.equals("+")){
-                resultado = Double.parseDouble(reserva) + Double.parseDouble(Resultado.getText().toString());
-                if(floor(resultado) == resultado){
-                    Resultado.setText(String.valueOf((int)Math.round(resultado)));
-                }else{
-                    Resultado.setText(String.valueOf(resultado));
-                }
+        }
+
+        if(operacion.equals("+")){
+            resultado = Double.parseDouble(memoria) + Double.parseDouble(Resultado.getText().toString());
+            if(floor(resultado) == resultado){
+                Resultado.setText(String.valueOf((int)Math.round(resultado)));
+            }else{
+                Resultado.setText(String.valueOf(resultado));
             }
-            if(operador.equals("/")){
-                resultado = Double.parseDouble(reserva) / Double.parseDouble(Resultado.getText().toString());
-                if(floor(resultado) == resultado){
-                    Resultado.setText(String.valueOf((int)Math.round(resultado)));
-                }else{
-                    Resultado.setText(String.valueOf(resultado));
-                }
+        }
+
+        if(operacion.equals("/")){
+            resultado = Double.parseDouble(memoria) / Double.parseDouble(Resultado.getText().toString());
+            if(floor(resultado) == resultado){
+                Resultado.setText(String.valueOf((int)Math.round(resultado)));
+            }else{
+                Resultado.setText(String.valueOf(resultado));
             }
-            if(operador.equals("*")){
-                resultado = Double.parseDouble(reserva) * Double.parseDouble(Resultado.getText().toString());
-                if(floor(resultado) == resultado){
-                    Resultado.setText(String.valueOf((int)Math.round(resultado)));
-                }else{
-                    Resultado.setText(String.valueOf(resultado));
-                }
+        }
+
+        if(operacion.equals("x")){
+            resultado = Double.parseDouble(memoria) * Double.parseDouble(Resultado.getText().toString());
+            if(floor(resultado) == resultado){
+                Resultado.setText(String.valueOf((int)Math.round(resultado)));
+            }else{
+                Resultado.setText(String.valueOf(resultado));
             }
+        }
     }
 }
